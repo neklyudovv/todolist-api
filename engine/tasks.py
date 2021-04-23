ef2a760461a0ad200a270bbeefac02e9f91b3812
Игорь Неклюдов <igor.nekludovv@gmail.com>
2021-04-23 17:19:15 +0300

    Удалил ненужные строки и комментарии

--- engine/tasks.py
+++ engine/tasks.py
@@ -18,9 +18,9 @@ def add_task(text, owner=''):
 			try:
 				db.session.add(new_task)
 				db.session.commit()
-				return jsonify(id=new_task.id, text=text, owner=owner), 200 # vse ok
+				return jsonify(id=new_task.id, text=text, owner=owner), 200
 			except:
-				return jsonify(msg='Ошибка базы данных'), 500 # smth went wrong
+				return jsonify(msg='Ошибка базы данных'), 500
 		return jsonify(msg='Пользователь не найден'), 404
 	return jsonify(msg='Текст задачи слишком короткий'), 401
 
@@ -29,8 +29,7 @@ def get_tasks(owner=''):
 	if check_user(owner):
 		try:
 			tasks = [{"id" : item.id, "text" : item.text, "owner" : item.owner} for item in models.Task.query.filter_by(owner=owner).distinct()] # генерирует список всех заметок
-			#tasks = [item.text for item in models.Task.query.filter_by(owner=owner).distinct()]
-			return jsonify(tasks) # переводим в JSON и возвращаем
+			return jsonify(tasks)
 		except:
 			return jsonify(tasks=[])
 	return jsonify(msg='Пользователь не найден'), 404
